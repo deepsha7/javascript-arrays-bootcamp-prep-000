c8ca958ca5705b680e25ccf754f4e311827197ba
Deepak Arora <deepsha898@gmail.com>
2017-10-22 20:35:15 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,2 +1,5 @@
 var  chocolateBars = ["snickers", "hundred grand", "kitkat","skittles"];
-var addElementToBeginningOfArray = chocolateBars.unshift("deepak");
+
+function addElementToBeginningOfArray(array, element){
+    return array.unshift(element);  
+}
